5e6b6bda990e2795fbc5617322bfbd24832e477c
Anders <anders1603@hotmail.com>
2019-10-02 08:50:11 +0200

    branch

--- jaoop1.js
+++ jaoop1.js
@@ -1,54 +1,43 @@
 'use strict';
 
-var $ = function (foo) {
-    return document.getElementById(foo);    // save keystrokes
-}
+let tunes = {
+  init: function(titel, composer, year, singer) {
+    this.titel = titel;
+    this.composer = composer;
+    this.year = year;
+    this.singer = [];
 
-let validate = function() {
-    if ($('name').value.length < 2) {
-      window.alert('Venligst skriv navn');
-      return false;
-    }
-    createCookie($('name').value, $('date').value, 0.0034222);
-    return true;
+  },
+  vis:function(){
+    return `${this.titel}, by ${this.composer}, from year ${this.year}, from ${this.singer}`;
   }
+};
 
-const init = function(){
-  $('submit').addEventListener('click', validate);
+const showTunes = function() {
+document.getElementById('placeholder').innerHTML = " ";
+  for (tunes of arr) {
+
+      document.getElementById('placeholder').innerHTML = tunes.vis() + '<br/>';
+
+  }
+};
+
+let arr = [];
+
+document.getElementById("send").addEventlistener('click', function() {
+  let t = Object.create(tunes);
+  t.init(title.value, composer.value, year.value, singer.value);
+  arr.push(t);
+  showTunes();
+});
+
+
+/*
+function start(){
+  addEventlistener('click', klik);
+
 }
-window.addEventListener('load', init);
+*/
 
 
-
-
-function createCookie(name, value, days) {
-    let expires;
-    if (days) {
-        let date = new Date();
-        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
-        expires = "; expires=" + date.toGMTString();
-    }
-    else {
-        expires = "";
-    }
-    document.cookie = name + "=" + value + expires + "; path=/";
-}
-
-function readCookie(name) {
-    let nameEQ = name + "=";
-    let ca = document.cookie.split(";");
-    for (let i = 0; i < ca.length; i++) {
-        let c = ca[i];
-        while (c.charAt(0) == " ") {
-            c = c.substring(1,c.length);
-        }
-        if (c.indexOf(nameEQ) == 0) {
-            return c.substring(nameEQ.length, c.length);
-        }
-    }
-    return null;
-}
-
-function eraseCookie(name) {
-    createCookie(name, "", -1);
-}
+window.addEventlistener('load', start);
